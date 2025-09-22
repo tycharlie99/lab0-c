e348da51f10f28e900165fea72201b3df1ef7ccc
TsungYu Chan <tycharlie.c@gmail.com>
2025-09-23 02:30:31 +0800

    Add the q_size function
    First commit for test the development flow
    Change-Id: Iad501d18ad63e7bf4528b44c2644a0b65a6d20b7

--- queue.c
+++ queue.c
@@ -1,8 +1,8 @@
+#include "queue.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#include "queue.h"
 
 /* Create an empty queue */
 struct list_head *q_new()
@@ -40,7 +40,15 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head)
+        return 0;
+
+    int len = 0;
+    struct list_head *cur;
+    list_for_each(cur, head)
+        len++;
+
+    return len;
 }
 
 /* Delete the middle node in queue */
